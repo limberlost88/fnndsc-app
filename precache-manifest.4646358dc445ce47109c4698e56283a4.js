20a140f6484c2adfc544b64e35abdfff80221bb7
root <root@localhost.localdomain>
2019-02-08 11:55:58 -0500

    Updates

---
+++ precache-manifest.4646358dc445ce47109c4698e56283a4.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "c53340cea8ee847a5a0b1e37137390d2",
+    "url": "/fnndsc-app/static/media/bostonchildrens.c53340ce.png"
+  },
+  {
+    "revision": "b6ce8b0586276dc3b825",
+    "url": "/fnndsc-app/static/css/main.f3df4e2e.chunk.css"
+  },
+  {
+    "revision": "35ac85a2f24da83c20e63957aaff498f",
+    "url": "/fnndsc-app/static/media/cookiemonster.35ac85a2.gif"
+  },
+  {
+    "revision": "895d8aacac75a448dd2a",
+    "url": "/fnndsc-app/static/js/1.895d8aac.chunk.js"
+  },
+  {
+    "revision": "ef15ebb33340dc976441",
+    "url": "/fnndsc-app/static/js/runtime~main.ef15ebb3.js"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/fnndsc-app/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/fnndsc-app/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/fnndsc-app/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/fnndsc-app/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "768a5c31b4f7a49bf548ecb7f3eaff40",
+    "url": "/fnndsc-app/static/media/sprites.768a5c31.png"
+  },
+  {
+    "revision": "8e9bfe9f3872adc6b1d2dad3ce738237",
+    "url": "/fnndsc-app/static/media/columbus.8e9bfe9f.gif"
+  },
+  {
+    "revision": "b6ce8b0586276dc3b825",
+    "url": "/fnndsc-app/static/js/main.b6ce8b05.chunk.js"
+  },
+  {
+    "revision": "bdf027131b1f84372d15d80d44f6b44f",
+    "url": "/fnndsc-app/static/media/docker.bdf02713.png"
+  },
+  {
+    "revision": "2fb6fd599c8d5bddf5be7414c514ee2d",
+    "url": "/fnndsc-app/static/media/redhat.2fb6fd59.png"
+  },
+  {
+    "revision": "ab5176d42c198f06152af5d54ecf383a",
+    "url": "/fnndsc-app/static/media/middle-AX_T2_FLAIR-AX_T2_FLAIR-233417.ab5176d4.jpg"
+  },
+  {
+    "revision": "81f45d74dd64ac34b75fda37b6cb9036",
+    "url": "/fnndsc-app/static/media/middle-COR_FSE_T2-COR_FSE_T2-233385.81f45d74.jpg"
+  },
+  {
+    "revision": "0e1004515d75ac4f0cdae5661cab95ab",
+    "url": "/fnndsc-app/static/media/middle-AX_TSE_T2__TRIO_PARAMETERS_-AX_TSE_T2__TRIO_PARAMETERS_-233573.0e100451.jpg"
+  },
+  {
+    "revision": "20963a76cb7dbc2688083070aabafa5b",
+    "url": "/fnndsc-app/static/media/evans.20963a76.gif"
+  },
+  {
+    "revision": "f4a6badee41a5f1a15873324ddf66d45",
+    "url": "/fnndsc-app/static/media/pratt.f4a6bade.gif"
+  },
+  {
+    "revision": "cefc850f43e85fb9c654796b92f40987",
+    "url": "/fnndsc-app/static/media/tucker.cefc850f.gif"
+  },
+  {
+    "revision": "895d8aacac75a448dd2a",
+    "url": "/fnndsc-app/static/css/1.dce04c91.chunk.css"
+  },
+  {
+    "revision": "b74c03a10f68095acd7a5ddce546c31a",
+    "url": "/fnndsc-app/index.html"
+  }
+];
